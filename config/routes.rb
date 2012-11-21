58befcdc3070fca8541a283d8fd50bcb8e286d74
Mihir Buch <mihirjb@gmail.com>
2012-11-21 14:05:16 +0530

    added help page with routes

--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,7 @@ Twitterapp::Application.routes.draw do
   
   match '/contact', :to =>'pages#contact'
    match '/about', :to =>'pages#about'
+   match '/help', :to =>'pages#help'
    
   get "pages/home"
   get "users/new"
